663e2727e01fe4b0928ecc584af544883962a6d7
Chaithanya Dileep THONDAMANATI <c.thondamanati@f5.com>
2024-04-17 12:37:16 +0530

    login is true

--- xc/xc_loadbalancer.tf
+++ xc/xc_loadbalancer.tf
@@ -276,9 +276,7 @@ resource "volterra_http_loadbalancer" "lb_https" {
           }
           flow_label {
             authentication {
-              login { 
-                disable_transaction_result {}
-              }
+              login = true
             }
           }
         }
